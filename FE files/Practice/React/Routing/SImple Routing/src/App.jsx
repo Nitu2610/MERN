34830843e83362a4db4ae22348280b581fa7a811
Nitu2610 <nikss26796@gmail.com>
2025-11-17 23:14:40 +0530

    add a features to dynamically add detail to inside the route and link

--- FE files/Practice/React/Routing/SImple Routing/src/App.jsx
+++ FE files/Practice/React/Routing/SImple Routing/src/App.jsx
@@ -10,6 +10,8 @@ import Playwear from "./pages/Playwear";
 import Sleepwear from "./pages/Sleepwear";
 import Footwear from "./pages/Footwear";
 import MensClothing from "./pages/MensClothing";
+import WomensClothing from "./pages/WomensClothing";
+
 import "./styles.css";
 import {
   BrowserRouter as Router,
@@ -19,14 +21,38 @@ import {
   Outlet,
   NavLink,
   Navigate,
+  Link,
 } from "react-router-dom";
 
+const routingDetails=[
+    {no:1,  to:'/', heading:'Home' , element: HomePage},
+    {no:2,  to:'/mens-clothing', heading:"Men's Clothing" ,  element:MensClothing},
+      {no:3,  to:'/women-mens-clothing', heading:"Women's Clothing ", element:WomensClothing},
+      {no:4,  to:'/kids-clothing', heading:'Kids Clothing' , element:KidsClothing},
+      {no:5,  to:'/Baby-clothing', heading:"Baby Clothing" , element:BabyClothing},
+      {no:6,  to:'/outwear', heading:"Outrwear" , element: Outerwear},
+      {no:7,  to:'/footwear', heading:"Footwear" , element:Footwear},
+      {no:8,  to:'/accessories', heading:"Accessories" , element:Accessories},]
+
 function MainNavigation() {
+  
   return (
-    <nav>
-      <Links to="/">Home</Links>
+    <nav >
+
+      {/* Manually creating the Link element.
+       <Links to="/">Home</Links>
       <Links to="/kids-clothing">Kids Clothing</Links>
-      <Links to="/mens-clothing">Men's Clothing</Links>
+      <Links to="/mens-clothing">Men's Clothing</Links> */}
+
+      { /* Dynamic Nav bar detaila */ }
+<ul style={{display:'flex', justifyContent:"space-evenly", alignItems:'center',}}>
+  
+      {routingDetails.map((e,i)=>{
+        let {no, to, heading}=e;
+        return <li key={no} style={{listStyle:'none'}}> <Link to={to}> {heading}</Link> </li>
+      })}
+</ul>
+
     </nav>
   );
 }
@@ -38,9 +64,22 @@ export default function App() {
       <MainNavigation />
     
         <Routes>
-          <Route path="/" element={ <HomePage/> }  />
-          <Route path="/kids-clothing" element={<KidsClothing/> }  />
-          <Route path="/mens-clothing" element={<MensClothing/>}  />
+          {/** Manually adding the Route */}
+
+        {/**
+         *   <Route path="/" element={<HomePage/>} />
+          <Route path="/kids-clothing" element={<KidsClothing/>} />
+          <Route path="/mens-clothing" element={<MensClothing/>} />
+         */}
+
+       {/** The Dynamically creating the Route and updating the vaues */}
+       
+         {
+          routingDetails.map((e,i)=>{
+            let {to,element:Element}=e;
+            return <Route path={to} element= { <Element />}/>
+          })
+         }
         </Routes>
      
     
