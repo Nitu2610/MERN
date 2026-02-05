4a4a63ba8b899c015ab4e6b5aada080579d83ed0
Nitu2610 <nikss26796@gmail.com>
2026-02-05 14:42:09 +0530

    feat: post model with user reference done.

--- BE files/Projects Hub/Blog Platform/models/posts.model.js
+++ BE files/Projects Hub/Blog Platform/models/posts.model.js
@@ -1,26 +1,26 @@
+const mongoose = require("mongoose");
 
+const postSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    location: String,
+    tag: [String],
+    coverImage: String,
+  },
+  {
+    timestamps: true,
+  },
+);
 
+const PostModel = mongoose.model("post", postSchema);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = PostModel;
 
 // Post model (mental picture)
 
@@ -33,3 +33,11 @@
 // location
 // tag
 // coverImage
+
+//  type: mongoose.Schema.Types.ObjectId,
+// ==>MongoDB creates a unique ID for every document. That ID looks like: 65f3a9c4e8...
+// This line says: “This field will store the ID of another document”
+
+//      ref: "user",
+//  ==> Tells Mongoose which collection this ID belongs to.
+// "user" must match: ==> mongoose.model("user", userSchema);
