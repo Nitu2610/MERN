c3193c777959d19774512d389a0b1dca2e17374f
Nitu2610 <nikss26796@gmail.com>
2026-06-28 10:53:25 +0530

    learning map concept.

---
+++ frontend_files/js/concept_problems/Map-Topic.js
@@ -0,0 +1,26 @@
+// Q1- Frequency Counter- Count how many times each element appears.
+// let arr= [1, 2, 1, 3, 2, 1];
+// let mp = new Map();
+
+// for( let key of arr){
+//   mp.set(key, (mp.get(key) || 0) +1 );
+// }
+// console.log(mp)
+//----------------------------------------------------------
+// Q2- First Repeating Element - Find the first element that repeats.
+// let char = "leetcode";
+
+// let mp = new Map();
+// for (let i = 0; i < char.length; i++) {
+//   mp.set(char[i], (mp.get(char[i]) || 0) + 1);
+// }
+
+// let res = null;
+// for (let [key, value] of mp) {
+//   if (value === 1) {
+//     res = key;
+//     break;
+//   }
+// }
+// console.log(res);
+//----------------------------------------------------------
